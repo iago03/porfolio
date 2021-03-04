54fcabd05ab4b4165480df6f9bcfc65e5fe92147
iago03 <71462411+iago03@users.noreply.github.com>
2021-03-04 11:50:58 +0400

    change works

--- src/app/work/work/work.component.ts
+++ src/app/work/work/work.component.ts
@@ -9,10 +9,10 @@ import { workCArd } from 'src/app/shared-file/shared-eduqation';
 export class WorkComponent implements OnInit {
   @Output() workCardAll:workCArd[] = [
     new workCArd("https://blogcastillo.netlify.app","./assets/img/blogcastillo.jpg","Blog Castillo"),
-    new workCArd("https://tik-tak-toi.netlify.app","./assets/img/tik-tak.jpg","tik-tak Game"),
+    new workCArd("https://nageboba.netlify.app","./assets/img/building.webp","buildin"),
     new workCArd("https://fashionbeautyblog.netlify.app","./assets/img/fashionblog.jpg","fashion beauty blog"),
-    new workCArd("https://freecolorgame.netlify.app","./assets/img/color-game.jpg","Color Game"),
-    new workCArd("https://phonenumberimitation.netlify.app","./assets/img/number.jpg","JS HTML and CSS"),
+    new workCArd("https://tik-tak-toi.netlify.app","./assets/img/tik-tak.jpg","tik-tak Game"),
+    new workCArd("https://calculatoriago.netlify.app","./assets/img/calculator.png","jquery HTML and CSS"),
     new workCArd("https://by-healthiest.netlify.app","./assets/img/healthiest.jpg","Healthy"),
   ]
   constructor() { }
